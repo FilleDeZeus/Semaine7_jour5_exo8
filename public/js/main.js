de2a795845fc34cb0a25109842dd66409ce53447
FilleDeZeus <enide26@hotmail.fr>
2023-01-16 12:22:50 +0100

    fin

---
+++ public/js/main.js
@@ -0,0 +1,33 @@
+let competences = {
+    HTML: '71%',
+    CSS: '95%',
+    JS : '50%',
+    React: '18%',
+    Laravel: '100%'
+}
+console.log(competences);
+//EXERCICE 1
+let elements = document.querySelectorAll("div#liste-competences > h2");
+console.log(elements);
+
+//EXERCICE 2
+
+for (const property in competences) {
+    elements.forEach(element => {
+        if(property == element.innerText){
+            element.textContent += ` ${competences[property]}`;
+
+            if (Number(competences[property].slice(0, -1))<50) {
+                element.style.backgroundColor = "red";
+
+            } else if (Number(competences[property].slice(0, -1))>50  && Number(competences[property].slice(0, -1))!=100){
+                element.style.backgroundColor = "green";
+                element.style.color ="white";
+
+            } else if (Number(competences[property].slice(0, -1))==100){
+                element.style.backgroundColor = "gold";
+            }
+        }
+        
+    });
+}
